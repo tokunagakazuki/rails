c14263c2203be846f474c160f5124237a6c9dc61
tokunagakazuki <tokutyaa@yahoo.co.jp>
2020-06-28 02:18:22 +0900

    ログイン機能とコメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
+  resources :comments, only: %i[create destroy]
   # get 'boards', to: 'boards#index'
   # get 'boards/new', to: 'boards#new'
   # post 'boards', to: 'board#create'
